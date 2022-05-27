d78ee9ea650a5dae0258d2fe17b27557afd067f6
Kuba <A446479@santandertecnologia.com.ar>
2022-05-27 10:58:50 -0300

    modificar para merge routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SideBar from './components/SideBar';
+import MainArea from './components/MainArea';
+import Home from './components/Home';
+import ProductsList from './components/ProductsList';
+import Product from './components/Product';
+import NewProduct from './components/NewProduct';
+import NotFound from './components/NotFound';
 
 
 function App() {
@@ -10,8 +17,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/products" element={<ProductsList/>}/>
-                <Route path="/products/:id" element={<ProductsList/>}/>
-                <Route path="/products/new" element={<NewProductView/>}/>
+                <Route path="/products/:id" element={<Product/>}/>
+                <Route path="/products/new" element={<NewProduct/>}/>
                 <Route path="*" element={<NotFound/>}/>
             </Routes>
 
