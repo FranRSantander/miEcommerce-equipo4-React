6b6c7e6c556d9d6db5a290540e3d78d1cb54087a
Kuba <A446479@santandertecnologia.com.ar>
2022-05-28 12:51:03 -0300

    aplique mobile first a listado de productos y agregue header con icono de menu, buscador y boton

--- src/components/MockCard.js
+++ src/components/MockCard.js
@@ -1,20 +1,11 @@
 import React from 'react'
 
-const style = {
-    border: "solid 1px black",
-    width:"1000px",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    paddingLeft: "8px", 
-    paddingRight: "8px"  
-
-}
-
 const mockCard = (props) => {
   return (
-    <div style={style}>
+    <div className="mockCard">
+  
       {props.children}
+      
     </div>
   )
 }
--- src/pages/Products/ProductsList/ProductsList.js
+++ src/pages/Products/ProductsList/ProductsList.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import MockCard from '../../../components/MockCard'
-import Header from '../../../components/MockHeader'
-
+import MockHeader from '../../../components/MockHeader'
 import "./ProductsList.css"
 import flecha_der from "../../../assets/flecha_der.svg"
 import{Link} from "react-router-dom"
+import "../../../components/MockCard.css"
+import "../../../components/MockHeader.css"
+import menu from "../../../assets/menu.png"
 
 
 
@@ -26,30 +28,39 @@ const products = [
 
 const ProductsList = () => {
   return (
-    <div className="productCard">
+    <div className="productDiv">
     {/*Header de Lista de Productos*/}  
-      <Header>
-        <div>Productos</div>
-        <div>
-          <input></input>
-          <button>Agregar Producto</button>
+      <MockHeader>
+        <div className="leftSideHeader">
+          <img src={menu} alt="menu" style={{height: "30px", width:"30px"}}/>
+          <div className="headerTitle">Productos</div>
         </div>
-      </Header>
+        
+
+        <div className="rightSideHeader">
+          <input className="inputBuscador" type="text" placeholder="Buscar productos..." />
+          <Link to="/products/new"><input className="botonAgregarProd" type="button" id="boton-agregar" value="Agregar Producto"/></Link>
+        </div>
+      </MockHeader>
 
     {/*Sección del Cuerpo de Lista de Productos*/}  
-
+    <div className="content">
     {products.map((product)=>{ 
       return <MockCard>
-              <div>       {/*div que contiene el detalle del producto  */}       
-                <img src={product.img}></img>
-                {product.name} <br></br>
-               #{product.id}
+              <div className="productInfo">       {/*div que contiene el detalle del producto  */}       
+                <img className="productImg" src={product.img} alt="product"></img>
+                <div>
+                  <h4 className="tituloProducto">{product.name}</h4>
+                  <span className="idNumber">#{product.id}</span>
+                </div>
               </div>
               <div>             {/*div que contiene la flechita  */}
-                <Link to="/"><img href= "/"src={flecha_der} style={{height: "6px", width: "6px"}}></img></Link>
+                <Link to="/"><img href= "/"src={flecha_der} alt="arrow" style={{height: "6px", width: "6px"}}></img></Link>
               </div>
             </MockCard>
     })}
+
+    </div>
     
     </div>
   )
