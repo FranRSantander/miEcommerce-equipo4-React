94f5dc7a1fb05b68c9e68ddd3a7b665f22f9c2f8
damianvelius <A446483@santandertecnologia.com.ar>
2022-05-30 14:59:32 -0300

    Hasta donde se llego

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from '../pages/Home/Home';
+import Home from '../pages/home/Home';
 import ProductsList from '../pages/Products/ProductsList/ProductsList';
 import NewProduct from '../pages/Products/NewProduct/NewProduct';
 
