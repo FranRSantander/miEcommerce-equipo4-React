9ae8c32ce5135835be8b958097e0788b6522940a
damianvelius <A446483@santandertecnologia.com.ar>
2022-06-03 17:21:53 -0300

    Header test passed render

--- src/__test__/Header.test.js
+++ src/__test__/Header.test.js
@@ -1,9 +1,9 @@
 import { render, screen} from "@testing-library/react"
 import Header from "../components/Header/Header"
 
-test("porfis", ()=>{
+test("Rendediza el Header", ()=>{
     render(<Header title="hola"><button>Click</button></Header>)
         screen.debug()
-            expect(screen.getByText("hola")).toBeInTheDocument()
-                expect(screen.getByRole("button")).toBeInTheDocument()
+        expect(screen.getByText("hola")).toBeInTheDocument()
+        expect(screen.getByRole("button")).toBeInTheDocument()
 })
--- src/__test__/SideBar.test.js
+++ src/__test__/SideBar.test.js
@@ -1,7 +1,8 @@
 import { render, screen} from "@testing-library/react"
 import SideBar from "../components/SideBar/SideBar"
 
-test("Renderiza", ()=>{
+test("Renderiza", () => {
     render(<SideBar/>)
-    screen.debug();
+        screen.debug();
+            expect(screen.getAllByLabelText("mi")).toBeInTheDocument()
 })
