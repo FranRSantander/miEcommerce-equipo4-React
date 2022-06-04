42ee5702af8ed9788d635aae72670ec0d4dbf06c
Kuba <A446479@santandertecnologia.com.ar>
2022-06-04 20:04:51 -0300

    moví código de mapeo de productos a otro componente para testearlo mejor

--- src/pages/Products/ProductsList/ProductsList.js
+++ src/pages/Products/ProductsList/ProductsList.js
@@ -1,26 +1,35 @@
+
 import React, {useState, useEffect} from 'react'
-import{Link} from "react-router-dom"
-import TarjetaCuerpo from '../../../components/Tarjeta/TarjetaCuerpo'
+import Tarjeta from '../../../components/Tarjeta/Tarjeta'
 import Header from '../../../components/Header/Header'
-import flecha from "../../../assets/chevron-right.svg"
 import "./ProductsList.css"
 import "../../../components/Tarjeta/Tarjeta.css"
 import "../../../components/Header/Header.css"
 import { getProducts } from '../../../utils/api'
+import { Link } from 'react-router-dom'
 
 const ProductsList = () => {
 
-  const [productos, setProductos] = useState([]);   
   const [inputValue, setInputValue] = useState("");
-  
-  useEffect(()=>{
-    getProducts()
-    .then(res=> res.json())
-    .then(data=>{
-      setProductos(data)
-    })
-  }, [])
 
+  function useProduct(){
+
+    const [productos, setProductos] = useState([]);   
+    
+    useEffect(()=>{
+      getProducts()
+      .then(res=> res.json())
+      .then(data=>{
+        setProductos(data)
+      })
+    }, [])
+
+    return {productos, inputValue}
+
+  }
+
+  const {productos} = useProduct();
+ 
   return (
     <div className="productDiv">
         {/*Header de Lista de Productos*/}  
@@ -28,35 +37,10 @@ const ProductsList = () => {
         <input className="inputBuscador" type="text" placeholder="Buscar productos..." onChange={(e)=>{setInputValue(e.target.value)}}/>
         <Link to="/products/new"><input className="botonAgregarProd" type="button" id="boton-agregar" value="Agregar Producto"/></Link>
       </Header>
-        {/*Sección del Cuerpo de Lista de Productos*/} 
-        {(productos.length > 0) ?
-          <div className="content">
-            {productos.filter(element =>{
-              if(inputValue === ""){
-                return element;
-              }else if(element.title.toLowerCase().includes(inputValue.toLowerCase())){
-                return element;
-              }else{
-                return null
-              }
-            }).map((product, i)=>{                                  
-            return <TarjetaCuerpo key={product.title+i}>
-                    <div className="productInfo">          
-                      <img className="productImg" src={product.image} alt="product" style={{height: "50px", width:"50px", borderRadius:"15px"}}></img>
-                      <div>
-                        <h4 className="tituloProducto">{product.title}</h4>
-                        <span className="idNumber">#{product.id}</span>
-                      </div>
-                    </div>
-                    <div>             {/*div que contiene la flechita  */}
-                      <Link to={`/products/${product.id}`}><img src={flecha} alt="arrow" /></Link>
-                    </div>
-                  </TarjetaCuerpo>
-        })}
-          </div>
-          :
-          <h1 style={{color:"white"}}>Cargando...</h1>
-        }
+      <Tarjeta 
+        productos={productos}
+        inputValue={inputValue}
+      />
     </div>
   )
 }
