c7b8a3be4c44c55684f7868cf6c9a9795b25a66f
Rechou <A446464@santandertecnologia.com.ar>
2022-06-06 14:42:18 -0300

    Agregando Test a SideBar(testeando Links)

--- src/components/SideBar/SideBar.test.js
+++ src/components/SideBar/SideBar.test.js
@@ -13,4 +13,16 @@ describe('SideBar', () => {
     })
 
     test('Rendriza el componente SideBar', () => {} )
+    
+    test('Link al inicio', () => {
+        expect(screen.getByText(/inicio/i).closest('a')).toHaveAttribute('href', '/')
+    })
+
+    test('Link a la vista de Productos', () => {
+        expect(screen.getByText(/productos/i).closest('a')).toHaveAttribute('href', '/products')
+    })
+
+    test('Link a la vista de Stores no implementada', () => {
+        expect(screen.getByText(/tiendas/i).closest('a')).toHaveAttribute('href', '/stores')
+    })
 })
