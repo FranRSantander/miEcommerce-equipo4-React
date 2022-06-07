03c3ea586c93e338a0d1c8c7a9ea71677a1b04b8
Kuba <A446479@santandertecnologia.com.ar>
2022-06-07 10:18:04 -0300

    test damian

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -3,7 +3,7 @@ import Header from "./Header"
 
 test("Rendediza el Header", ()=>{
     render(<Header title="hola"><button>Click</button></Header>)
-        screen.debug()
+       
         expect(screen.getByText("hola")).toBeInTheDocument()
         expect(screen.getByRole("button")).toBeInTheDocument()
 })
